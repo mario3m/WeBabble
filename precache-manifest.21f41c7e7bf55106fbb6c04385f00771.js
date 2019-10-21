19ce3d990fd4e80b9fad6a726da36b6b3aa9cd18
Mario <mariomunoz3m@usal.es>
2019-10-21 10:19:34 +0200

    1

---
+++ precache-manifest.21f41c7e7bf55106fbb6c04385f00771.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "622b9d6a345f427beb5e",
+    "url": "/css/app.0d866287.css"
+  },
+  {
+    "revision": "622b9d6a345f427beb5e",
+    "url": "/js/app.e3110ed0.js"
+  },
+  {
+    "revision": "7f7fa50838b9e800f8c3",
+    "url": "/css/chunk-vendors.157edb0b.css"
+  },
+  {
+    "revision": "7f7fa50838b9e800f8c3",
+    "url": "/js/chunk-vendors.463cd5b2.js"
+  },
+  {
+    "revision": "4ac192dcd64a7a4e1a310e7c415d45ba",
+    "url": "/img/home_background.4ac192dc.jpg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "847bdfd3bf6caa8b547fb7b6f3a3e2a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
