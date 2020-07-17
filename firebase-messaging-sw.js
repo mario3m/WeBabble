8f7ac4e700e41ac76a86627081d812149286aa01
Mario Muñoz Moreno <mariomunoz3m@usal.es>
2020-07-17 18:28:53 +0200

    20200717_01

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.db060e3d9776a76df4cb39a34ceb5574.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.bcbc6dfcf977715d09bebf875031990f.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts('https://www.gstatic.com/firebasejs/5.6.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.6.0/firebase-messaging.js');
